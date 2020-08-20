45a0fd9cd14147a5227db68a30040311da1af8e1
Jessica Dior <jessicaclairdior@gmail.com>
2020-08-20 01:55:49 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -1,7 +1,7 @@
-def introduction(josh)
-  puts "Hi, my name is Josh.\n"
+def introduction(name)
+  puts "Hi, my name is #{name}."
 end
 
-def introduction_with_language(dan, ember.js)
-  puts "Hi, my name is Dan and I am learning to program in Ember.js.\n"
+def introduction_with_language(name, language.js)
+  puts "Hi, my name is #{name} and I am learning to program in #{language}."
 end
